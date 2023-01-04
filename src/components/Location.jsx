846320fcca37784b0fc91125037be03570dc8f06
Emilien Gerbouin <emilien.gerbouin@gmail.com>
2023-01-04 16:35:04 +0100

    modify style

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -4,21 +4,20 @@ import Accordion from "./Accordion"
 import ImageSlider from "./ImageSlider";
 const logements = require("../logements.json");
 const star = require ("../assets/images/star.png")
-const orangeStar = require ("../assets/images/orangeStar.png")
+// const orangeStar = require ("../assets/images/orangeStar.png")
 
 const Location = () => {
   const params = useParams()
 
-  const aze = [1, 2, 3, 4]
+  // const aze = [1, 2, 3, 4]
 
   const foundLogement = logements.find(l => {
     return l.id === params.id;
   })
 
-  const equipement = foundLogement.equipments.map((e) => (
-    <li>{e}</li>
+  const equipement = foundLogement.equipments.map((e, index) => (
+    <li key={index}>{e}</li>
   ))
-
   return (
     <div>
       <div className="containerStyles">
@@ -33,8 +32,8 @@ const Location = () => {
           <h1 className="house_title">{foundLogement.title}</h1>
           <p className="location_house">{foundLogement.location}</p>
           <ul className="tags_house">
-            {foundLogement.tags.map((tag) => (
-              <li className="tag">{tag}</li>
+            {foundLogement.tags.map((tag, index) => (
+              <li key={index} className="tag">{tag}</li>
               ))}
           </ul>
         </div>
@@ -74,40 +73,6 @@ const Location = () => {
         </div>
       </div>
     </div>
-    // <div>
-    //   <div className="containerStyles">
-    //     <ImageSlider slides={foundLogement.pictures} />
-    //   </div>
-
-    //   <div className="infos_location">
-    //     <div className="space-between">
-    //       <h1 className="house_title">{foundLogement.title}</h1>
-    //       <div className="host_infos">
-    //         <p className="host_name">{foundLogement.host.name}</p>
-    //         <img src={foundLogement.host.picture} alt=""></img>
-    //       </div>
-    //     </div>
-    //     <p className="location_house">{foundLogement.location}</p>
-    //   </div>
-
-    //   <div className="space-between">
-    //     <ul className="tags_house">
-    //       {foundLogement.tags.map((tag) => (
-    //         <li className="tag">{tag}</li>
-    //       ))}
-    //     </ul>
-    //     <div>{foundLogement.rating}</div>
-    //   </div>
-      
-    //   <div className="small__accordions">
-    //     <div className="width_small_accordion">
-    //       <Accordion title="Description" content={foundLogement.description}/>
-    //     </div>
-    //     <div className="width_small_accordion">
-    //       <Accordion title="Equipements" content={equipement}/>
-    //     </div>
-    //   </div>
-    // </div>
   );
 };
 
